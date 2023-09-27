c456652837dede1f99fb1ca95b84a9aeb2099817
NazarMural <nazarmural777@gmail.com>
2023-09-27 17:43:50 +0300

    Додає swagger

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 // const authRouter = require("./routes/api/auth");
 // const contactsRouter = require("./routes/api/contacts");
@@ -15,6 +17,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // app.use("/api/contacts", contactsRouter);
 // app.use("/api/users", authRouter);
